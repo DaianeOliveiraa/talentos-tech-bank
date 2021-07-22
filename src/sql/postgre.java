039cac1b919fb74981977a82bb43daa885653612
Daiane Oliveira <daianeldeoliveira@gmail.com>
2021-07-22 17:13:05 -0300

    Aula 21 - SQL

---
+++ src/sql/postgre.java
@@ -0,0 +1,32 @@
+package sql;
+
+import java.sql.*;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class postgre {
+    public static void main(String[] args) {
+        String urlPostgres = System.getenv("urlPostgres");
+        String usuario = System.getenv("usuario");
+        String senha = System.getenv("senha");
+
+        try (
+                Connection conn = DriverManager.getConnection(urlPostgres, usuario, senha);
+                Statement stmt = conn.createStatement()) {
+
+            String query = "select 1 as id, 'Daiane' as nome";
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("id"));
+                System.out.println("NOME: " + rs.getString("nome"));
+            }
+            rs.close();
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
